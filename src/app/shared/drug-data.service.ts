d1fba24b7f6b381ed3407484195fcad8adcba98b
Wei Fang <wfang.ca@gmail.com>
2022-01-25 08:44:13 -0500

    modified service call

--- src/app/shared/drug-data.service.ts
+++ src/app/shared/drug-data.service.ts
@@ -8,32 +8,22 @@ import { Drug } from '../shared/drug-data.model';
 })
 
 export class DrugDataService {
-    // for docke-compose demo
-    //host = 'localhost';
-    //port = 8080;
-    
-    // for minikube demo
-    //host = '192.168.99.107';
-    //port = 31408;
-
-    // for minishift demo
-    //host = 'dpd-server-ts-myproject.192.168.99.100.nip.io';  // take the route of the dpd-server
-    //port = 80;
-    
-    // for openshift test
-    // host and port will be replace by tekton pipelines
-    host = 'DPD_SERVER_HOSTNAME'; 
-    port = 80;
 
     constructor(private http: HttpClient) {
         this.http = http;
     }
 
     getDrugByBrandName(serachText: string) : Observable<Drug[]> {
-        // return an Observable for a list of drugs by brand name
-        // return this.http.get<Drug[]>('http://' + this.host + ':' + this.port + '/api/drugs/brand_name/' + serachText)
-        // knative testing
-        let url: string = 'https://' + this.host + ':' + this.port + '/api/drugs/brand_name/' + serachText;
+        // for openshift test
+        // host and port will be replace by tekton pipelines
+        let host: string = 'DPD_SERVER_HOSTNAME'; 
+        let port: number = 80;
+
+        let url: string = 'http://' + host + ':' + port + '/api/drugs/brand_name/' + serachText;
+        if( host.startsWith("http")){
+            url = host + ':' + port + '/api/drugs/brand_name/' + serachText;
+        }
+        
         return this.http.get<Drug[]>(url)
     }
   
